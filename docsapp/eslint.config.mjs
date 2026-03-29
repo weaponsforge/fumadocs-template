3bd7d39e51e2986e8d106963e8f16c4501d6f58e
weaponsforge <forgeweapons@gmail.com>
2026-03-29 11:09:07 +0800

    chore: replace biome linter with eslint
    - Uninstall @biomejs/biome
    - Install and setup eslint for nextjs
    - Run npm audit fix

---
+++ docsapp/eslint.config.mjs
@@ -0,0 +1,80 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import importPlugin from 'eslint-plugin-import'
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    '.source/**',
+    'out/**',
+    'build/**',
+    'node_modules/**',
+    'next-env.d.ts',
+  ]),
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      'indent': ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'never'],
+      'no-unused-vars': 'error',
+      'no-undef': 'error',
+      'no-console': ['error', { allow: ['error'] }],
+      'react/react-in-jsx-scope': 'off',
+      'no-restricted-imports': [
+        'error',
+        { 'patterns':
+          [
+            '@/features/*',
+            './features/*'
+          ]
+        }
+      ],
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling', 'index'],
+            'type',
+          ],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: 'next/**',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '@/**',
+              group: 'internal',
+              position: 'after',
+            },
+          ],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+      'import/newline-after-import': ['error', { count: 1 }],
+    },
+  },
+])
+
+export default eslintConfig
